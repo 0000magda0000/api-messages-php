53933b96106959b6d459ab531c41f947f306561d
Magda Barthofer <magda.barthofer@gmail.com>
2021-02-10 18:27:21 +0100

    update db config again

--- config/database.php
+++ config/database.php
@@ -25,12 +25,3 @@ class Database{
 }
 ?>
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-?>
