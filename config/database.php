d76aec93e54605b0ff55478d49a34d7ba52929e6
Magda Barthofer <magda.barthofer@gmail.com>
2021-02-10 22:48:13 +0100

    added schema

--- config/database.php
+++ config/database.php
@@ -1,37 +1,38 @@
 <?php
-//Get Heroku ClearDB connection information
-$url = parse_url(getenv("DATABASE_URL"));
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($server, $username, $password, $db);
+// //Get Heroku ClearDB connection information
+// $url = parse_url(getenv("DATABASE_URL"));
+// $server = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $db = substr($url["path"],1);
+// $active_group = 'default';
+// $query_builder = TRUE;
+// // Connect to DB
+// $conn = mysqli_connect($server, $username, $password, $db);
 
-// class Database{
+class Database{
 
-//     // specify database credentials
-//     private $host = $url["host"];
-//     private $db_name = substr($url["path"], 1);
-//     private $username = $url["user"];
-//     private $password = $url["pass"];
-//     public $conn;
+    // specify database credentials
+    private $url = parse_url(getenv('mysql://bbc08eb1da307f:c478dc6e@us-cdbr-east-03.cleardb.com/heroku_394a42a4b5cb062?reconnect=true');
+    private $host = $url["host"];
+    private $db_name = substr($url["path"], 1);
+    private $username = $url["user"];
+    private $password = $url["pass"];
+    public $conn;
 
-//     // get the database connection
-//     public function getConnection(){
+    // get the database connection
+    public function getConnection(){
 
-//         $this->conn = null;
+        $this->conn = null;
 
-//         try{
-//             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-//             $this->conn->exec("set names utf8");
-//         }catch(PDOException $exception){
-//             echo "Connection error: " . $exception->getMessage();
-//         }
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
 
-//         return $this->conn;
-//     }
-// }
+        return $this->conn;
+    }
+}
 ?>
