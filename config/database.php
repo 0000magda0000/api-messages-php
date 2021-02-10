a9fc81d28cc9a20842a6ef7e0dee935c8a3e80c8
Magda Barthofer <magda.barthofer@gmail.com>
2021-02-10 19:35:07 +0100

    changed db conf

--- config/database.php
+++ config/database.php
@@ -1,29 +1,37 @@
 <?php
-class Database{
+//Get Heroku ClearDB connection information
+$url = parse_url(getenv("DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($server, $username, $password, $db);
 
-    // specify your own database credentials
-    private $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    private $host = $url["host"];
-    private $db_name = substr($url["path"], 1);
-    private $username = $url["user"];
-    private $password = $url["pass"];
-    public $conn;
+// class Database{
 
-    // get the database connection
-    public function getConnection(){
+//     // specify database credentials
+//     private $host = $url["host"];
+//     private $db_name = substr($url["path"], 1);
+//     private $username = $url["user"];
+//     private $password = $url["pass"];
+//     public $conn;
 
-        $this->conn = null;
+//     // get the database connection
+//     public function getConnection(){
 
-        try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
-        }catch(PDOException $exception){
-            echo "Connection error: " . $exception->getMessage();
-        }
+//         $this->conn = null;
 
-        return $this->conn;
-    }
-}
+//         try{
+//             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+//             $this->conn->exec("set names utf8");
+//         }catch(PDOException $exception){
+//             echo "Connection error: " . $exception->getMessage();
+//         }
+
+//         return $this->conn;
+//     }
+// }
 ?>
-<?php
-
